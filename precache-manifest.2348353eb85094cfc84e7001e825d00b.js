5c1bf7442490e54633e2da0edaab62b5aa46ab3f
MAX AMMO <maxammo@MAXsMacBook-Pro.local>
2019-06-19 21:58:33 +0100

    Updates

---
+++ precache-manifest.2348353eb85094cfc84e7001e825d00b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3471d7acb9d4d5eb008a2f4a04a21d84",
+    "url": "/drinkingCardsUnits/index.html"
+  },
+  {
+    "revision": "448f0edeaf7010ecc7ba",
+    "url": "/drinkingCardsUnits/static/css/main.a767af72.chunk.css"
+  },
+  {
+    "revision": "010791ffeb2bb73b71fb",
+    "url": "/drinkingCardsUnits/static/js/2.61665a80.chunk.js"
+  },
+  {
+    "revision": "448f0edeaf7010ecc7ba",
+    "url": "/drinkingCardsUnits/static/js/main.6cb701f2.chunk.js"
+  },
+  {
+    "revision": "b6067599c44cad75955c",
+    "url": "/drinkingCardsUnits/static/js/runtime~main.4790f248.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/drinkingCardsUnits/static/media/logo.5d5d9eef.svg"
+  }
+]);
