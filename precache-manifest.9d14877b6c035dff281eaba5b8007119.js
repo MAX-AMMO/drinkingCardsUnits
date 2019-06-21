b1924ffbff51c4ebcc3da591c149c804ea7ea4f2
MAX AMMO <maxammo@MAXsMacBook-Pro.local>
2019-06-21 12:09:22 +0100

    Updates

--- precache-manifest.9d14877b6c035dff281eaba5b8007119.js
+++ precache-manifest.9d14877b6c035dff281eaba5b8007119.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2309bc2c7d45cbd1910c210f9cd61df4",
+    "revision": "0efc8197ab488ce02624b3747c9f3ac8",
     "url": "/index.html"
   },
   {
-    "revision": "b1e464c2c63b3ddbf86d",
-    "url": "/static/css/main.554d6a14.chunk.css"
+    "revision": "33d042209ebbf330802f",
+    "url": "/static/css/main.e6aaa4b1.chunk.css"
   },
   {
     "revision": "1071f7cabe4c1212ae04",
     "url": "/static/js/2.b74f3dba.chunk.js"
   },
   {
-    "revision": "b1e464c2c63b3ddbf86d",
+    "revision": "33d042209ebbf330802f",
     "url": "/static/js/main.13c6385e.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d2c4e8ee6524906f2052a16289aac6ab.js"
+  "/precache-manifest.9d14877b6c035dff281eaba5b8007119.js"
 );
 
 self.addEventListener('message', (event) => {
