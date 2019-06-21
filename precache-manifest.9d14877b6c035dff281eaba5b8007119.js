3382ee895403d57f452ad6d932f447fa44e3cca3
MAX AMMO <maxammo@MAXsMacBook-Pro.local>
2019-06-21 12:20:35 +0100

    Updates

---
+++ precache-manifest.9d14877b6c035dff281eaba5b8007119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efc8197ab488ce02624b3747c9f3ac8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33d042209ebbf330802f",
+    "url": "/static/css/main.e6aaa4b1.chunk.css"
+  },
+  {
+    "revision": "1071f7cabe4c1212ae04",
+    "url": "/static/js/2.b74f3dba.chunk.js"
+  },
+  {
+    "revision": "33d042209ebbf330802f",
+    "url": "/static/js/main.13c6385e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/drinkingCardsUnits/precache-manifest.f45b12ed93e40f42dd0409fe2a9eb289.js"
+  "/precache-manifest.9d14877b6c035dff281eaba5b8007119.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/drinkingCardsUnits/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
